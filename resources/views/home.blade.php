e567a199c9fc4e09c36b8d392e7a8c3c1389e50d
Neeraj-Shah-dev <wp86.department@gmail.com>
2025-05-28 12:38:41 +0530

    Updated login dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-{{-- @extends('layouts.app')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -20,44 +20,4 @@
         </div>
     </div>
 </div>
-@endsection --}}
-
-
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    <span>{{ __('Dashboard') }}</span>
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-danger btn-sm">Logout</button>
-                    </form>
-                </div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <p>{{ __('You are logged in as:') }}</p>
-                    <ul>
-                        <li><strong>Name:</strong> {{ Auth::user()->name }}</li>
-                        <li><strong>Email:</strong> {{ Auth::user()->email }}</li>
-                    </ul>
-
-                    <hr>
-
-                    <p>Welcome to your dashboard!</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
-s
